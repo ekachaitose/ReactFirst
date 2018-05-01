d4acbad215a8a20048c82903d71e7a3e71c66fca
ekachaitose <ekachaitose11@gmail.com>
2018-05-01 23:13:42 +0700

    create event and set state send value

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import Header from './header'
 import Footer from './footer'
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentTime : 0
+    }
+    this.handleFooterClicked = this.handleFooterClicked.bind(this)
+  }
+  handleFooterClicked(time){
+    this.setState({currentTime: time})
+  }
   render() {
+    let {currentTime} = this.state
     return (
       <div>
         <Header currentUser="Ekkachai" isLoggedIn/>
-        <div>Hello</div>
-        <Footer/>
+        <div>currentTime  = {currentTime}</div>
+        <Footer onTimerClicked={this.handleFooterClicked}/>
       </div>
     )
   }
